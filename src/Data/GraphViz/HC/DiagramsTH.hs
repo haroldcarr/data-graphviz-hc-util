35e6e14ecbb30004403d6552b0085b01af8d0209
Harold Carr <harold.carr@gmail.com>
2020-02-09 15:09:10 -0700

    more args (to support svg tooltips)

--- src/Data/GraphViz/HC/DiagramsTH.hs
+++ src/Data/GraphViz/HC/DiagramsTH.hs
@@ -6,12 +6,26 @@ import           Language.Haskell.TH
 
 type FunctionName = String
 type DiagramLabel = String
+type ToolTip      = String
 
 mk :: String -> [(FunctionName, DiagramLabel)] -> Q [Dec]
 mk fname0 = return . map mkBinding
   where
     mkBinding (fName, label) =
         ValD (VarP (mkName fName))
-             (NormalB (AppE (AppE (VarE (mkName fname0)) (LitE (StringL fName)))
-                            (LitE (StringL label)))) []
+             (NormalB (AppE (AppE (VarE (mkName fname0))
+                                  (LitE (StringL fName)))
+                            (LitE (StringL label))))
+             []
+
+mk3 :: String -> [(FunctionName, DiagramLabel, ToolTip)] -> Q [Dec]
+mk3 fname0 = return . map mkBinding
+ where
+  mkBinding (fName, label, tt) =
+        ValD (VarP (mkName fName))
+             (NormalB (AppE (AppE (AppE (VarE (mkName fname0))
+                                        (LitE (StringL fName)))
+                                  (LitE (StringL label)))
+                            (LitE (StringL tt))))
+             []
 
